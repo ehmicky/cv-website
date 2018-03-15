183b54b97d928e0282c857ccf69dbb1cb2d72857
ehmicky <ehmicky@gmail.com>
2018-03-15 14:25:53 +0100

    Add default AWS_PROFILE

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scotty --source www.mickael-hebert.com --bucket www.mickael-hebert.com --update --website --region eu-west-1
+AWS_PROFILE=ehmicky scotty --source www.mickael-hebert.com --bucket www.mickael-hebert.com --update --website --region eu-west-1
