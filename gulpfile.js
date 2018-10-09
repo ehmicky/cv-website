375435cd030aa2e508ab59a5f5bac586432d2f5b
ehmicky <ehmicky@gmail.com>
2018-10-09 11:01:33 +0200

    Upgrade dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var wait = require('gulp-wait');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+'use strict'
 
-gulp.task('scripts', function() {
-    return gulp.src('js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
+const { src, dest, watch, parallel } = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+
+const scripts = function() {
+    return src('js/scripts.js')
+        .pipe(uglify({ output: { comments: '/^!/' } }))
         .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
-});
+        .pipe(dest('js'));
+};
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
+const styles = function () {
+    return src('./scss/styles.scss')
         .pipe(wait(250))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
+        .pipe(dest('./css'));
+};
 
-gulp.task('watch', ['scripts', 'styles'], function() {
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('scss/*.scss', ['styles']);
-});
+const watchJs = function(){
+  return watch('js/*.js', scripts)
+}
+const watchScss = function(){
+  return watch('scss/*.scss', styles)
+}
+const watchTask = parallel([watchJs, watchScss])
+
+module.exports = {
+  scripts,
+  styles,
+  watch: watchTask,
+}
