1351e948f5d58c14a3d1ee6bbc8c42946fcfb351
ehmicky <ehmicky@gmail.com>
2019-08-25 20:58:41 +0200

    Fix linting

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,10 @@
-module.exports.handler = function(event, context, callback) {
+// eslint-disable-next-line promise/prefer-await-to-callbacks, import/unambiguous
+const handler = function(event, context, callback) {
+  // eslint-disable-next-line promise/prefer-await-to-callbacks
   callback(null, { statusCode: 200, body: 'Hello world' })
+}
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+  handler,
 }
