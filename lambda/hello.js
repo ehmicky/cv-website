dee89a9a77a13f67020a8ace93ea39f713e47754
ehmicky <ehmicky@gmail.com>
2019-09-01 15:20:56 +0200

    Try using netlify-lambda

---
+++ lambda/hello.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line promise/prefer-await-to-callbacks, import/unambiguous
+const handler = function(event, context, callback) {
+  // eslint-disable-next-line promise/prefer-await-to-callbacks, node/prefer-global/process, no-restricted-globals
+  callback(null, { statusCode: 200, body: `Node ${process.version}` })
+}
+
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+  handler,
+}
