3244d627922afec79678ecbcd11e2c7f4e300701
ehmicky <ehmicky@gmail.com>
2019-09-01 15:29:29 +0200

    Use async function

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,6 @@
 import { version } from 'process'
-import { callbackify } from 'util'
 
 // eslint-disable-next-line require-await, no-unused-vars
-export const handler = callbackify(async function handle(event, context) {
+export const handler = async function(event, context) {
   return { statusCode: 200, body: `Node ${version}` }
-})
+}
