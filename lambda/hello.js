733bb64e8ddf53ca292152ffef3b23832c58a2c4
ehmicky <ehmicky@gmail.com>
2019-09-01 15:22:50 +0200

    Improve function

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,10 +1,5 @@
-// eslint-disable-next-line promise/prefer-await-to-callbacks, import/unambiguous
-const handler = function(event, context, callback) {
-  // eslint-disable-next-line promise/prefer-await-to-callbacks, node/prefer-global/process, no-restricted-globals
-  callback(null, { statusCode: 200, body: `Node.js ${process.version}` })
-}
+import { version } from 'process'
 
-// eslint-disable-next-line import/no-commonjs
-module.exports = {
-  handler,
+export const handler = function(event, context, callback) {
+  callback(null, { statusCode: 200, body: `Node.js ${version}` })
 }
