2036237f27a68e184b9eaf7544113f9e4bddb37b
ehmicky <ehmicky@gmail.com>
2019-09-01 15:29:48 +0200

    Add hook

---
+++ lambda/deploy-building.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line require-await, no-unused-vars, filenames/match-regex, unicorn/filename-case
+export const handler = async function(event, context) {
+  return { statusCode: 200, body: 'Deploy building' }
+}
