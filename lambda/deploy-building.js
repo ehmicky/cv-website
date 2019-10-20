2641e76ca2796b1a72b1c0530beac58ed47d669b
ehmicky <ehmicky@gmail.com>
2019-10-20 15:44:17 +0200

    Update test functions

--- lambda/deploy-building.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line require-await, no-unused-vars, filenames/match-regex, unicorn/filename-case
-export const handler = async function(event, context) {
-  return { statusCode: 200, body: 'Deploy building' }
-}
--- lambda/test/test.js
+++
@@ -1,6 +0,0 @@
-import { version } from 'process'
-
-// eslint-disable-next-line require-await, no-unused-vars
-export const handler = async function(event, context) {
-  return { statusCode: 200, body: `Test Node.js ${version}` }
-}
