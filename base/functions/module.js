70ce25e324169d13d9c889aab662bfe033fbd3f0
ehmicky <ehmicky@gmail.com>
2020-03-15 15:56:41 +0100

    Test

--- base/functions/module.js
+++
@@ -1,6 +0,0 @@
-const avg = require('math-avg')
-
-module.exports.handler = async function(event, context) {
-  const number = avg([2, 4])
-  return { statusCode: 200, body: `${number} ${Date.now()}` }
-}
