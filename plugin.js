0be0cb86d71c4d93b241f5d270e43c0269c47d41
ehmicky <ehmicky@gmail.com>
2020-06-23 22:06:40 +0200

    Revert "Debuggin"
    This reverts commit 6ce6df397f6bff2bbc475d639cbb5f4d13c43a3d.

--- plugin.js
+++
@@ -1,9 +0,0 @@
-const del = require('del')
-
-module.exports = {
-  async onBuild({utils}) {
-    await del('.', {force: true})
-    console.log(await utils.cache.list())
-    console.log(utils.git)
-  }
-}
