bbbac931892f575ce670510b65704eac082bd637
ehmicky <ehmicky@gmail.com>
2020-07-26 11:51:59 +0200

    Try

--- functions/core.js
+++ functions/core.js
@@ -1,4 +1,5 @@
 const { version } = require('process')
+require('@ehmicky/eslint-config')
 
 module.exports.handler = async function(event, context) {
   return { statusCode: 200, body: `Node.js ${version} ${Date.now()}` }
