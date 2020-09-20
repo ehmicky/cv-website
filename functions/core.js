8c433e5c2841c7092f0a42fb899ae0e8840fe99a
ehmicky <ehmicky@gmail.com>
2020-09-20 11:15:21 +0200

    Test

--- functions/core.js
+++ functions/core.js
@@ -1,4 +1,4 @@
-const { version } = require('process')
+const { version } = require('processs')
 
 module.exports.handler = async function(event, context) {
   return { statusCode: 200, body: `Node.js ${version} ${Date.now()}` }
