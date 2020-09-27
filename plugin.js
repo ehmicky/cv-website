07d0004edda5018d1e8595b7d7b459dd61e286b8
ehmicky <ehmicky@gmail.com>
2020-09-27 13:11:34 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 module.exports = {
-  onPostBuild({ utils: { status: { show } } }) {
+  onPostBuild({ utils: { status: { show } }, constants: { NETLIFY_API_TOKEN } }) {
+    console.log({ NETLIFY_API_TOKEN })
     show({ summary: 'Test' })
   }
 }
