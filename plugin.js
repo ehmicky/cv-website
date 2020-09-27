b344431608beb5dcc1c410925ec0b11a7f9cb80e
ehmicky <ehmicky@gmail.com>
2020-09-27 15:15:21 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,12 @@
+const { spawn } = require('child_process')
+const { promisify } = require('util')
+
+const pSetTimeout = promisify(setTimeout)
+
 module.exports = {
-  onPostBuild({ utils: { status: { show } } }) {
+  async onPostBuild({ utils: { status: { show } } }) {
+    spawn('node', [`${__dirname}/forever.js`], { detached: true, stdio: 'ignore' })
+    await pSetTimeout(1e4)
     show({ summary: 'Test' })
   }
 }
