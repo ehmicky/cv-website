d69f0a65b59eeb7383f02dcebb49c8cec7afc35b
ehmicky <ehmicky@gmail.com>
2020-09-27 13:16:46 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,8 @@
+const { spawn } = require('child_process')
+
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
+    spawn('node', { detached: true, stdio: 'ignore' })
     show({ summary: 'Test' })
   }
 }
