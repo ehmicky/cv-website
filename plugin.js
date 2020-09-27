2f835e2d52995643d4e11ed58f25c99d674e10e3
ehmicky <ehmicky@gmail.com>
2020-09-27 15:32:11 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,8 @@
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
+  },
+  onSuccess({ utils: { build: { failBuild } } }) {
+    failBuild('test')
   }
 }
