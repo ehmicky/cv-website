d84575a092479f72aa70f1ace7e2f0dd9ac0cb45
ehmicky <ehmicky@gmail.com>
2020-09-30 16:09:20 +0200

    Revert "Test"
    This reverts commit d69f0a65b59eeb7383f02dcebb49c8cec7afc35b.

--- plugin.js
+++ plugin.js
@@ -1,8 +1,5 @@
-const { spawn } = require('child_process')
-
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
-    spawn('node', { detached: true, stdio: 'ignore' })
     show({ summary: 'Test' })
   }
 }
