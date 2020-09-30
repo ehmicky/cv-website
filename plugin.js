5c2bd4aa79c39eaa1ac2e6e96146259def41a93e
ehmicky <ehmicky@gmail.com>
2020-09-30 14:32:01 +0200

    Revert "Test"
    This reverts commit 07d0004edda5018d1e8595b7d7b459dd61e286b8.

--- plugin.js
+++ plugin.js
@@ -1,6 +1,5 @@
 module.exports = {
-  onPostBuild({ utils: { status: { show } }, constants: { NETLIFY_API_TOKEN } }) {
-    console.log({ NETLIFY_API_TOKEN })
+  onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
   }
 }
