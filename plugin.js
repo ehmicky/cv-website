d209710e643b67ceab7d5290c74ccac4a7bd1d86
ehmicky <ehmicky@gmail.com>
2020-10-01 18:22:50 +0200

    Revert "Test"
    This reverts commit b344431608beb5dcc1c410925ec0b11a7f9cb80e.

--- plugin.js
+++ plugin.js
@@ -1,12 +1,5 @@
-const { spawn } = require('child_process')
-const { promisify } = require('util')
-
-const pSetTimeout = promisify(setTimeout)
-
 module.exports = {
-  async onPostBuild({ utils: { status: { show } } }) {
-    spawn('node', [`${__dirname}/forever.js`], { detached: true, stdio: 'ignore' })
-    await pSetTimeout(1e4)
+  onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
   }
 }
