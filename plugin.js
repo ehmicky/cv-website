e002ee8877248f6021e6662355c6b375ad71b437
ehmicky <ehmicky@gmail.com>
2020-10-01 18:26:39 +0200

    Revert "Test"
    This reverts commit 2f835e2d52995643d4e11ed58f25c99d674e10e3.

--- plugin.js
+++ plugin.js
@@ -1,8 +1,5 @@
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
-  },
-  onSuccess({ utils: { build: { failBuild } } }) {
-    failBuild('test')
   }
 }
