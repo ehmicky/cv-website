a0aa9d281ae684f46f5fcb07d2ce7cc1e5259faf
ehmicky <ehmicky@gmail.com>
2020-10-02 20:23:41 +0200

    Revert "Demo site"
    This reverts commit b2ecfad0b001dc0d67ad8c68ee0e907eb1baef40.

--- plugin.js
+++ plugin.js
@@ -1,13 +1,5 @@
 module.exports = {
-  onPostBuild() {
-    console.log('onPostBuild')
-  },
-  onSuccess() {
-    console.log('onSuccess')
-    // Throw new Error('onSuccess error')
-  },
-  onEnd() {
-    console.log('onEnd')
-    // Throw new Error('onEnd error')
-  },
+  onPostBuild({ utils: { status: { show } } }) {
+    show({ summary: 'Test' })
+  }
 }
