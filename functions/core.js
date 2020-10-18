c6f91ba2b554cde8580f7f384f30126e485f7f23
ehmicky <ehmicky@gmail.com>
2020-10-18 17:14:11 +0200

    Test

--- functions/core.js
+++
@@ -1,5 +0,0 @@
-const { version } = require('process')
-
-module.exports.handler = async function(event, context) {
-  return { statusCode: 200, body: `Node.js ${version} ${Date.now()}` }
-}
--- functions/parent.js
+++
@@ -1,5 +0,0 @@
-const { name } = require('../package.json')
-
-module.exports.handler = async function(event, context) {
-  return { statusCode: 200, body: `${name} ${Date.now()}` }
-}
