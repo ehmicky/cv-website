0d9344680892aa0a7dd4a2186e9e22e8824bf7bf
ehmicky <ehmicky@gmail.com>
2020-10-27 12:17:03 +0100

    Revert "Test"
    This reverts commit 3ad7f2260c46d21c75c39da1c13a06b1fa90d2c8.

--- functions/time.js
+++
@@ -1,8 +0,0 @@
-const now = require('./dynamic/thing')
-exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}! It is ${now()}`,
-  }
-}
