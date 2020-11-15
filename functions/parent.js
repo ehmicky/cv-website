2d2489c6c93cbd044d3d65a6c09ca428e1df0684
ehmicky <ehmicky@gmail.com>
2020-11-15 17:45:06 +0100

    Test

--- functions/parent.js
+++ functions/parent.js
@@ -1,4 +1,5 @@
 const { name } = require('../package.json')
+require('faunadb')
 
 module.exports.handler = async function(event, context) {
   return { statusCode: 200, body: `${name} ${Date.now()}` }
