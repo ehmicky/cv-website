d500a15ac43d06cebd6fc73fe33fb97e99e6bf27
ehmicky <ehmicky@gmail.com>
2020-11-15 23:13:11 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 module.exports = {
-  onPostBuild({ utils: { status: { show } } }) {
+  onPostBuild({ utils: { status: { show } }, constants: { FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC } }) {
+    console.log({ FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC })
     show({ summary: 'Test' })
   }
 }
