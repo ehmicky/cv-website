4c4a65f5c4053e30a0e3533f8bcd9ff7f7fddbad
ehmicky <ehmicky@gmail.com>
2020-11-15 23:25:22 +0100

    Test

--- functions/parent.js
+++ functions/parent.js
@@ -1,4 +1,4 @@
-const { name } = require('../package.json')
+const { name } = require('../package.jsons')
 
 module.exports.handler = async function(event, context) {
   return { statusCode: 200, body: `${name} ${Date.now()}` }
