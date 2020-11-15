2924a1f3e208b4f6d903e6f80dcfef38fb90b1c0
ehmicky <ehmicky@gmail.com>
2020-11-15 23:13:24 +0100

    Test again

--- plugin.js
+++ plugin.js
@@ -1,4 +1,13 @@
+const { rename } = require('fs')
+const { promisify } = require('util')
+
+const pRename = promisify(rename)
+
 module.exports = {
+  async onPreBuild({ constants: { FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC } }) {
+    console.log({ FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC })
+    await pRename('funcs', 'netlify-automatic-functions')
+  },
   onPostBuild({ utils: { status: { show } }, constants: { FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC } }) {
     console.log({ FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC })
     show({ summary: 'Test' })
