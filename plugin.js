d90a2fde9f0966ed8942f63d6610df1d24eac531
ehmicky <ehmicky@gmail.com>
2020-11-16 15:18:48 +0100

    Revert "Test again"
    This reverts commit b55b3a3776251ae1e2a704f5eca82a68619b314f.

--- plugin.js
+++ plugin.js
@@ -1,13 +1,4 @@
-const { rename } = require('fs')
-const { promisify } = require('util')
-
-const pRename = promisify(rename)
-
 module.exports = {
-  async onPreBuild({ constants: { FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC } }) {
-    console.log({ FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC })
-    await pRename('funcs', 'netlify-automatic-functions')
-  },
   onPostBuild({ utils: { status: { show } }, constants: { FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC } }) {
     console.log({ FUNCTIONS_SRC, FUNCTIONS_DIST, EDGE_HANDLERS_SRC })
     show({ summary: 'Test' })
