0695bd39031d9403c89d0899edb64d0d7ef26431
ehmicky <ehmicky@gmail.com>
2020-11-20 21:02:19 +0100

    Revert "Test"
    This reverts commit 2d2489c6c93cbd044d3d65a6c09ca428e1df0684.

--- functions/parent.js
+++ functions/parent.js
@@ -1,5 +1,4 @@
 const { name } = require('../package.json')
-require('faunadb')
 
 module.exports.handler = async function(event, context) {
   return { statusCode: 200, body: `${name} ${Date.now()}` }
