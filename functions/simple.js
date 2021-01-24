829fd0a33a99626239007559388e59597cdbf7cc
ehmicky <ehmicky@gmail.com>
2021-01-24 15:40:08 +0100

    Try chromium

--- functions/simple.js
+++ functions/simple.js
@@ -1,3 +1,54 @@
-module.exports.handler = async function(event, context) {
-  return { statusCode: 200, body: String(Date.now()) }
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
+
+exports.handler = async (event, context, callback) => {
+  let theTitle = null
+  let browser = null
+  console.log('spawning chrome headless')
+  try {
+    const executablePath = await chromium.executablePath
+
+    // setup
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: executablePath,
+      headless: chromium.headless,
+    })
+
+    // Do stuff with headless chrome
+    const page = await browser.newPage()
+    const targetUrl = 'https://davidwells.io'
+
+    // Goto page and then do stuff
+    await page.goto(targetUrl, {
+      waitUntil: ["domcontentloaded", "networkidle0"]
+    })
+
+    await page.waitForSelector('#phenomic')
+
+    theTitle = await page.title();
+
+    console.log('done on page', theTitle)
+
+  } catch (error) {
+    console.log('error', error)
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error
+      })
+    })
+  } finally {
+    // close browser
+    if (browser !== null) {
+      await browser.close()
+    }
+  }
+
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      title: theTitle,
+    })
+  })
 }
