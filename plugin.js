fedda8aba14bb9a8f2b4a702f44811bba4872608
ehmicky <ehmicky@gmail.com>
2021-02-03 21:35:08 +0100

    Revert "Test"
    This reverts commit fd75cf941458e1468bbdcad821482579bb47c797.

--- plugin.js
+++ plugin.js
@@ -1,9 +1,5 @@
-const { promisify } = require('util')
-const pSetTimeout = promisify(setTimeout)
-
 module.exports = {
-  async onSuccess({ utils: { status: { show } } }) {
-    await pSetTimeout(6e4)
+  onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
   }
 }
