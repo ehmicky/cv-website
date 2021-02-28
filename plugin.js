8b665fe5598707f0d06d532b2975ca3e62d805a4
ehmicky <ehmicky@gmail.com>
2021-02-28 23:14:45 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,7 @@
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
-    show({ summary: 'Test' })
+    const link = '\x1b]8;;https://www.netlify.com\x07Netlify\x1b]8;;\x07'
+    console.log(link)
+    show({ summary: `Test ${link}` })
   }
 }
