dfa6380f8e8d9545ac727ff61d640cfb73c1b635
ehmicky <ehmicky@gmail.com>
2021-02-28 23:15:01 +0100

    Test link

--- plugin.js
+++ plugin.js
@@ -1,5 +1,8 @@
+const terminalLink = require('terminal-link')
+
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
+    console.log(terminalLink('Netlify', 'https://www.netlify.com'))
     show({ summary: 'Test' })
   }
 }
