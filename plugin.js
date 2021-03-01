a762c86387329c11c0cc36b75781905a1fca551e
ehmicky <ehmicky@gmail.com>
2021-03-01 14:59:23 +0100

    Revert "Test"
    This reverts commit 8b665fe5598707f0d06d532b2975ca3e62d805a4.

--- plugin.js
+++ plugin.js
@@ -1,7 +1,5 @@
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
-    const link = '\x1b]8;;https://www.netlify.com\x07Netlify\x1b]8;;\x07'
-    console.log(link)
-    show({ summary: `Test ${link}` })
+    show({ summary: 'Test' })
   }
 }
