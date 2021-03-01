53307b7e79f9a670f335d2dad1ab0c7af2a6cb53
ehmicky <ehmicky@gmail.com>
2021-03-01 15:04:36 +0100

    Revert "Test link"
    This reverts commit dfa6380f8e8d9545ac727ff61d640cfb73c1b635.

--- plugin.js
+++ plugin.js
@@ -1,8 +1,5 @@
-const terminalLink = require('terminal-link')
-
 module.exports = {
   onPostBuild({ utils: { status: { show } } }) {
-    console.log(terminalLink('Netlify', 'https://www.netlify.com'))
     show({ summary: 'Test' })
   }
 }
