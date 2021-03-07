b9801a78642a8b581c995f1ac6db15a0e02e11c6
ehmicky <ehmicky@gmail.com>
2021-03-07 13:42:34 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 module.exports = {
-  onPostBuild({ utils: { status: { show } } }) {
+  onPostBuild({ utils: { status: { show } }, constants: { CACHE_DIR } }) {
+    console.log({ CACHE_DIR })
     show({ summary: 'Test' })
   }
 }
