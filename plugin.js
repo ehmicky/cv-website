d551210735f46298ff6a46e57970f337d0f0e592
ehmicky <ehmicky@gmail.com>
2021-03-10 17:55:23 +0100

    Revert "Test lingering processes"
    This reverts commit d590931c57c074c0b2f609be7f6432eb872a18ec.

--- plugin.js
+++ plugin.js
@@ -1,13 +1,5 @@
-const { spawn } = require('child_process')
-const { promisify } = require('util')
-
-const pSetTimeout = promisify(setTimeout)
-
 module.exports = {
-  async onPostBuild({ utils: { status: { show } } }) {
-    spawn('node', [`${__dirname}/forever.js`], { detached: true, stdio: 'ignore' })
-    await pSetTimeout(1e4)
-    console.log('Test')
+  onPostBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
   }
 }
