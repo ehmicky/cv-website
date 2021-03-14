6ad6cdc7f8da74b3ce3845cae40c3c52eb3ca9c4
ehmicky <ehmicky@gmail.com>
2021-03-14 13:15:02 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,8 @@
 module.exports = {
-  onPostBuild({ utils: { status: { show } } }) {
+  onPostBuild({ utils: { status: { show }, build: { failBuild } } }) {
+    const error = new Error('try this')
+    error.customType = 'this custom type'
+    failBuild('Try that', { error })
     show({ summary: 'Test' })
   }
 }
