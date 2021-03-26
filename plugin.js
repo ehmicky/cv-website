a192a4830ef8f719bdb6c6692b05ef22956439aa
ehmicky <ehmicky@gmail.com>
2021-03-26 17:27:55 +0100

    Revert "Try child process"
    This reverts commit 98a421233bda2bbfb30b3d96c6a297a9d71503ee.

--- plugin.js
+++ plugin.js
@@ -1,8 +1,5 @@
-const { spawn } = require('child_process')
-
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
-    spawn('sleep', ['60'], { detached: true })
     show({ summary: 'Test' })
   }
 }
