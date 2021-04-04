88d41248f1ff5ecce1aa7d110809625752ba84ad
ehmicky <ehmicky@gmail.com>
2021-04-04 11:49:30 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,6 +1,9 @@
+const AnsiUp = require('ansi_up')
+const ansiUp = new AnsiUp.default
+
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
-    console.log(']8;;http://www.google.com\x07b b')
+    console.log(ansiUp.ansi_to_html(']8;;http://www.google.com\x07b b'))
     show({ summary: 'Test' })
   }
 }
