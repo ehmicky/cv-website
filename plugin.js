7e65bbb032f9454bc9df582b77eb9ac99452bdaf
ehmicky <ehmicky@gmail.com>
2021-04-04 13:18:16 +0200

    Remove debug

--- plugin.js
+++ plugin.js
@@ -1,8 +1,5 @@
-const hyperlinker = require('hyperlinker')
-
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
-    console.log(hyperlinker('Netlify', 'https://www.netlify.com'))
     show({ summary: 'Test' })
   }
 }
