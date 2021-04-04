0b0688f34f9c9138cd3dec8abc53d0a65f70ce08
ehmicky <ehmicky@gmail.com>
2021-04-04 17:35:30 +0200

    Try terminal link

--- plugin.js
+++ plugin.js
@@ -1,7 +1,11 @@
 const { spawn } = require('child_process')
+const { link } = require('ansi-escapes')
 
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
+    console.log(`
+These processes have been terminated. In case this creates a problem for your build, refer to this ${link('article', 'https://answers.netlify.com/t/support-guide-how-to-address-the-warning-message-related-to-terminating-processes-in-builds/35277')} for details about why this process termination happens and how to fix it.
+    `)
     spawn('sleep', ['60'], { detached: true })
     show({ summary: 'Test' })
   }
