3965c106f0fa4c2f7b5c49ebf808b246556adb42
ehmicky <ehmicky@gmail.com>
2021-04-04 11:44:31 +0200

    Test OSC hyperlinks

--- plugin.js
+++ plugin.js
@@ -1,5 +1,8 @@
+const { link } = require('ansi-escapes');
+
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
+    console.log(link('some text', 'https://example.com') + '   <--- command + click here!');
     show({ summary: 'Test' })
   }
 }
