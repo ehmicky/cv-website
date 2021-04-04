1a115b65e0355976690c3a40cd5aff78febe860e
ehmicky <ehmicky@gmail.com>
2021-04-04 11:47:59 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 module.exports = {
   onPreBuild({ utils: { status: { show } } }) {
-    console.log('\x1B]8;;a b\x07a\x1B]8;;\x07')
+    console.log('\x1B]8;;a b\x07\x1B]8;;\x07')
     show({ summary: 'Test' })
   }
 }
