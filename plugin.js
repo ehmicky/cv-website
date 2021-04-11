610ee830e7492617b0ef6e1240847a60a7145ce6
ehmicky <ehmicky@gmail.com>
2021-04-11 13:23:58 +0200

    Test terminal links

--- plugin.js
+++ plugin.js
@@ -1,5 +1,11 @@
+const { link } = require('ansi-escapes')
+const terminalLink = require('terminal-link')
+
 module.exports = {
-  onPreBuild({ utils: { status: { show } } }) {
-    show({ summary: 'Test' })
+  onPostBuild() {
+    console.log('https://www.netlify.com/')
+    console.log('\x1B]8;;https://www.netlify.com\x07Netlify\x1B]8;;\x07')
+    console.log(link('Netlify', 'https://www.netlify.com/'))
+    console.log(terminalLink('Netlify', 'https://www.netlify.com/'))
   }
 }
