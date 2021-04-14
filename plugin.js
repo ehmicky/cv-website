900bcf89be0a3dcbab911f98f78af5a42bb841a9
ehmicky <ehmicky@gmail.com>
2021-04-14 18:17:50 +0200

    Revert "Test terminal links"
    This reverts commit 610ee830e7492617b0ef6e1240847a60a7145ce6.

--- plugin.js
+++ plugin.js
@@ -1,11 +1,5 @@
-const { link } = require('ansi-escapes')
-const terminalLink = require('terminal-link')
-
 module.exports = {
-  onPostBuild() {
-    console.log('https://www.netlify.com/')
-    console.log('\x1B]8;;https://www.netlify.com\x07Netlify\x1B]8;;\x07')
-    console.log(link('Netlify', 'https://www.netlify.com/'))
-    console.log(terminalLink('Netlify', 'https://www.netlify.com/'))
+  onPreBuild({ utils: { status: { show } } }) {
+    show({ summary: 'Test' })
   }
 }
