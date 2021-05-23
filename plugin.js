e64816b6a9ab6c2b33d549cfa9d0b6c3ba3b5d91
ehmicky <ehmicky@gmail.com>
2021-05-23 11:35:52 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
 module.exports = {
-  onPreBuild({ utils: { status: { show } } }) {
+  onPreBuild({ utils: { status: { show } }, netlifyConfig }) {
+    console.log(netlifyConfig)
     show({ summary: 'Test' })
   }
 }
