1ad31c12d64ceb978e08827a70b6299addbb8d33
ehmicky <ehmicky@gmail.com>
2021-05-23 13:31:51 +0200

    Test

--- plugin.js
+++ plugin.js
@@ -1,5 +1,15 @@
+const { rename } = require('fs')
+const { promisify } = require('util')
+
+const pRename = promisify(rename)
+
 module.exports = {
-  onPreBuild({ utils: { status: { show } } }) {
+  async onPreBuild({ utils: { status: { show } }, constants }) {
+    console.log(constants)
+    await pRename('netlify/functions_test', 'netlify/functions')
     show({ summary: 'Test' })
+  },
+  onBuild({ constants }) {
+    console.log(constants)
   }
 }
