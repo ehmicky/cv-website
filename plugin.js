2b21a62a68e82377b109e233b45e553c16d3b6e3
ehmicky <ehmicky@gmail.com>
2021-05-25 19:12:10 +0200

    Revert "Test"
    This reverts commit e64816b6a9ab6c2b33d549cfa9d0b6c3ba3b5d91.

--- plugin.js
+++ plugin.js
@@ -1,6 +1,5 @@
 module.exports = {
-  onPreBuild({ utils: { status: { show } }, netlifyConfig }) {
-    console.log(netlifyConfig)
+  onPreBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
   }
 }
