49ce01ba1f26bf2a80fc2e8fefa3fc23c5caedf4
ehmicky <ehmicky@gmail.com>
2021-05-26 18:23:48 +0200

    Revert "Test"
    This reverts commit 1ad31c12d64ceb978e08827a70b6299addbb8d33.

--- plugin.js
+++ plugin.js
@@ -1,15 +1,5 @@
-const { rename } = require('fs')
-const { promisify } = require('util')
-
-const pRename = promisify(rename)
-
 module.exports = {
-  async onPreBuild({ utils: { status: { show } }, constants }) {
-    console.log(constants)
-    await pRename('netlify/functions_test', 'netlify/functions')
+  onPreBuild({ utils: { status: { show } } }) {
     show({ summary: 'Test' })
-  },
-  onBuild({ constants }) {
-    console.log(constants)
   }
 }
