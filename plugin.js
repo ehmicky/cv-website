ed48ba6d7da4d92855bbf5fdfdc592e9ee4b787d
ehmicky <ehmicky@gmail.com>
2021-06-13 11:52:00 +0200

    Fix linting

--- plugin.js
+++ plugin.js
@@ -1,5 +1,10 @@
+// eslint-disable-next-line unicorn/prefer-module, import/no-commonjs, import/unambiguous
 module.exports = {
-  onPreBuild({ utils: { status: { show } } }) {
+  onPreBuild({
+    utils: {
+      status: { show },
+    },
+  }) {
     show({ summary: 'Test' })
-  }
+  },
 }
