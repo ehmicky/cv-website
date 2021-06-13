5a89e29d482955ed779a4e3a78eb6ae62483faf5
ehmicky <ehmicky@gmail.com>
2021-06-13 15:32:59 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -1,6 +1,6 @@
 // eslint-disable-next-line unicorn/prefer-module, import/no-commonjs, import/unambiguous
 module.exports = {
-  onPreBuild({
+  onSuccess({
     utils: {
       status: { show },
     },
