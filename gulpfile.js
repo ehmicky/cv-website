87f68a7962dbaf264f331ba15674f5ea03370b4f
ehmicky <ehmicky@gmail.com>
2021-06-13 11:52:33 +0200

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,35 @@
-// eslint-disable-next-line import/no-unresolved, node/no-missing-import
-export * from '@ehmicky/dev-tasks'
+// eslint-disable-next-line node/no-unpublished-import
+import del from 'del'
+// eslint-disable-next-line node/no-extraneous-import
+import { src, dest, watch as watchFiles, series } from 'gulp'
+// eslint-disable-next-line node/no-unpublished-import
+import gulpUglify from 'gulp-uglify'
+
+const SRC_DIR = 'src'
+const BUILD_DIR = 'build'
+const JS_FILES = `${SRC_DIR}/**/*.js`
+const OTHER_FILES = [
+  `${SRC_DIR}/{css,images,libs,fonts}/**`,
+  `${SRC_DIR}/favicon.ico`,
+  `${SRC_DIR}/index.html`,
+]
+
+const clean = async function () {
+  // TODO: replace with `fs.promises.rm(..., {recursive: true})` after
+  // dropping support for Node <14
+  await del(BUILD_DIR)
+}
+
+const uglify = function () {
+  return src(JS_FILES).pipe(gulpUglify()).pipe(dest(BUILD_DIR))
+}
+
+const move = function () {
+  return src(OTHER_FILES).pipe(dest(BUILD_DIR))
+}
+
+export const build = series(clean, move, uglify)
+
+export const watch = function () {
+  return watchFiles(`${SRC_DIR}/**`, build)
+}
