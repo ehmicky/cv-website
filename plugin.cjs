73c4a11609c232873b32b57a1d66dc7060760ed6
ehmicky <ehmicky@gmail.com>
2021-06-27 23:12:46 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -2,8 +2,10 @@ module.exports = {
   onPreBuild({
     utils: {
       status: { show },
+      build: { cancelBuild },
     },
   }) {
     show({ summary: 'Test' })
+    cancelBuild('test')
   },
 }
