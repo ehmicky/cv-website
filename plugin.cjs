dc97edd1f1b82c8a5b6210cbcd762d4a51def6d4
ehmicky <ehmicky@gmail.com>
2021-07-04 23:41:14 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -3,7 +3,10 @@ module.exports = {
     utils: {
       status: { show },
     },
+    netlifyConfig,
   }) {
     show({ summary: 'Test' })
+    netlifyConfig.build.processing.images = {}
+    netlifyConfig.build.processing.images.compress = true
   },
 }
