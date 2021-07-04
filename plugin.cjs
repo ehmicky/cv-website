d9440bb3113394f3cecfe0b147134b2a1d31ddc4
ehmicky <ehmicky@gmail.com>
2021-07-04 13:48:05 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -1,9 +1,22 @@
+const { readdir } = require('fs')
+const { promisify } = require('util')
+
+const pReaddir = promisify(readdir)
+
 module.exports = {
   onPreBuild({
     utils: {
       status: { show },
     },
+    netlifyConfig,
   }) {
     show({ summary: 'Test' })
+    netlifyConfig.redirects = [...netlifyConfig.redirects, { from: '/one', to: '/two' }]
+  },
+  async onPostBuild({ constants: { PUBLISH_DIR } }) {
+    console.log(await pReaddir(PUBLISH_DIR))
+  },
+  async onSuccess({ constants: { PUBLISH_DIR } }) {
+    console.log(await pReaddir(PUBLISH_DIR))
   },
 }
