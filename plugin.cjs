90b46fa3a6deb12d961bea69bb156ab7f43d9830
ehmicky <ehmicky@gmail.com>
2021-07-06 14:37:33 +0200

    Revert "Test"
    This reverts commit b430da273380502b33a44cd82351ea00ae767e27.

--- plugin.cjs
+++ plugin.cjs
@@ -3,12 +3,7 @@ module.exports = {
     utils: {
       status: { show },
     },
-    netlifyConfig,
   }) {
     show({ summary: 'Test' })
-    netlifyConfig.build.command = "gulp build && echo test"
-  },
-  onBuild({ netlifyConfig }) {
-    console.log(netlifyConfig.build.command)
   },
 }
