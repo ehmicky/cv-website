75c862c71e6106d001783c71637d885a1ab1cb47
ehmicky <ehmicky@gmail.com>
2021-08-08 15:37:53 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -11,7 +11,6 @@ module.exports = {
     netlifyConfig,
   }) {
     show({ summary: 'Test' })
-    netlifyConfig.redirects.push({ from: '/one', to: '/two' })
     netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
   },
 }
