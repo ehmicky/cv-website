c7b34c32356b862ce15c2ce3427d5d84caa63bdc
ehmicky <ehmicky@gmail.com>
2021-08-08 13:42:22 +0200

    Test no plugins

--- plugin.cjs
+++ plugin.cjs
@@ -1,17 +1,9 @@
-const { readdir } = require('fs')
-const { promisify } = require('util')
-
-const pReaddir = promisify(readdir)
-
 module.exports = {
   onPreBuild({
     utils: {
       status: { show },
     },
-    netlifyConfig,
   }) {
     show({ summary: 'Test' })
-    netlifyConfig.redirects.push({ from: '/one', to: '/two' })
-    netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
   },
 }
