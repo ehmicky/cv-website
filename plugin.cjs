c3e47abae3cb979e905671d569022ff28b551f29
ehmicky <ehmicky@gmail.com>
2021-08-11 20:35:21 +0200

    Revert "Test no plugins"
    This reverts commit c7b34c32356b862ce15c2ce3427d5d84caa63bdc.

--- plugin.cjs
+++ plugin.cjs
@@ -1,9 +1,17 @@
+const { readdir } = require('fs')
+const { promisify } = require('util')
+
+const pReaddir = promisify(readdir)
+
 module.exports = {
   onPreBuild({
     utils: {
       status: { show },
     },
+    netlifyConfig,
   }) {
     show({ summary: 'Test' })
+    netlifyConfig.redirects.push({ from: '/one', to: '/two' })
+    netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
   },
 }
