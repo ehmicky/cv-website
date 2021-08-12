49fa3cfa7ccf446f9856dd2db302e1603e03acf8
ehmicky <ehmicky@gmail.com>
2021-08-12 19:39:36 +0200

    Revert "Test"
    This reverts commit 75c862c71e6106d001783c71637d885a1ab1cb47.

--- plugin.cjs
+++ plugin.cjs
@@ -11,6 +11,7 @@ module.exports = {
     netlifyConfig,
   }) {
     show({ summary: 'Test' })
+    netlifyConfig.redirects.push({ from: '/one', to: '/two' })
     netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
   },
 }
