8149508a36b9ba024ad8f18deda21a308eea2254
ehmicky <ehmicky@gmail.com>
2021-08-18 18:17:19 +0200

    Revert "Demo"
    This reverts commit 8deb6694cb98e7452b113061d28fc3082ac221a8.

--- plugin.cjs
+++ plugin.cjs
@@ -1,15 +1,17 @@
-module.exports = {
-  onPostBuild({ netlifyConfig: { redirects, headers } }) {
-    console.log('Redirects', redirects)
-    console.log('Headers', headers)
+const { readdir } = require('fs')
+const { promisify } = require('util')
 
-    redirects.push({
-      from: '/this_path',
-      to: '/that_path',
-    })
-    headers.push({
-      for: '/example_path',
-      values: { HEADER_NAME: 'header_value' },
-    })
+const pReaddir = promisify(readdir)
+
+module.exports = {
+  onPreBuild({
+    utils: {
+      status: { show },
+    },
+    netlifyConfig,
+  }) {
+    show({ summary: 'Test' })
+    netlifyConfig.redirects.push({ from: '/one', to: '/two' })
+    netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
   },
 }
