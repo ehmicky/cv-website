f5d7c4969752d38e1d8a43c67740ae42b1a99b47
ehmicky <ehmicky@gmail.com>
2021-09-12 17:19:59 +0200

    Test

--- plugin.cjs
+++ plugin.cjs
@@ -2,11 +2,13 @@ module.exports = {
   onPreBuild({
     utils: {
       status: { show },
+      git,
     },
     netlifyConfig,
   }) {
     show({ summary: 'Test' })
     netlifyConfig.redirects.push({ from: '/one', to: '/two' })
     netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
+    console.log(git)
   },
 }
