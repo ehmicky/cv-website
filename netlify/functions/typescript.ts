373f935cebbe971a0a0eebf5279c6b7215831b4e
ehmicky <ehmicky@gmail.com>
2021-09-12 11:30:34 +0200

    Test

--- netlify/functions/typescript.ts
+++ netlify/functions/typescript.ts
@@ -1,4 +1,4 @@
-export const handler = async function () {
+exporttt const handler = async function () {
   const statusCode: number = 200
   const body: string = String(Date.now())
   return { statusCode, body }
