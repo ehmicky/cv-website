e5fc02dc4809266395a7342bbb731ac1bb7d866a
ehmicky <ehmicky@gmail.com>
2021-09-17 16:01:39 +0200

    Revert "Test"
    This reverts commit f5d7c4969752d38e1d8a43c67740ae42b1a99b47.

--- plugin.cjs
+++ plugin.cjs
@@ -2,13 +2,11 @@ module.exports = {
   onPreBuild({
     utils: {
       status: { show },
-      git,
     },
     netlifyConfig,
   }) {
     show({ summary: 'Test' })
     netlifyConfig.redirects.push({ from: '/one', to: '/two' })
     netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
-    console.log(git)
   },
 }
