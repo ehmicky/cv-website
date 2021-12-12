c88bb4b0e9db420e657a4f984952673466d5ef3d
ehmicky <ehmicky@gmail.com>
2021-12-12 13:27:22 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,9 +1,18 @@
-export const onPreBuild = function({
+export const onPreBuild = async function({
   utils: {
+    run,
+    build: { failBuild },
+    functions,
+    cache,
     status: { show },
   },
   netlifyConfig,
 }) {
+  await run('echo', ['oh'])
+  await run.command('echo ah')
+  console.log(await functions.list())
+  console.log(cache.save.toString())
+  failBuild('test')
   show({ summary: 'Test' })
   netlifyConfig.redirects.push({ from: '/one', to: '/two' })
   netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
