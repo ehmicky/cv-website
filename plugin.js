de9a38a59f456da647381b118548c6e6e48518b4
ehmicky <ehmicky@gmail.com>
2021-12-12 17:17:29 +0100

    Test

--- plugin.js
+++ plugin.js
@@ -1,9 +1,11 @@
 export const onPreBuild = function({
+  constants,
   utils: {
     status: { show },
   },
   netlifyConfig,
 }) {
+  console.log(constants.NETLIFY_BUILD_VERSION)
   show({ summary: 'Test' })
   netlifyConfig.redirects.push({ from: '/one', to: '/two' })
   netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
