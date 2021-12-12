e8573069be0b8c68278fa73cf5c15a84808496e7
ehmicky <ehmicky@gmail.com>
2021-12-12 13:27:09 +0100

    Remove debug

--- plugin.js
+++ plugin.js
@@ -4,9 +4,6 @@ export const onPreBuild = function({
   },
   netlifyConfig,
 }) {
-  console.log(process.env.DEPLOY_URL)
-  console.log(process.env.DEPLOY_PRIME_URL)
-  console.log(process.env.SITE_NAME)
   show({ summary: 'Test' })
   netlifyConfig.redirects.push({ from: '/one', to: '/two' })
   netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
