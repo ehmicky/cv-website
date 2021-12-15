b2eb6ae8cd4d11f47ef05be994de67d052779ea4
ehmicky <ehmicky@gmail.com>
2021-12-15 22:16:35 +0100

    Revert "Test"
    This reverts commit 94802d31bc361bc9698bd303534cf7ffb179956a.

--- plugin.js
+++ plugin.js
@@ -1,13 +1,9 @@
-export const onPreBuild = async function({
-  constants,
+export const onPreBuild = function({
   utils: {
-    cache,
     status: { show },
   },
   netlifyConfig,
 }) {
-  console.log(constants.CACHE_DIR)
-  console.log(await cache.list())
   show({ summary: 'Test' })
   netlifyConfig.redirects.push({ from: '/one', to: '/two' })
   netlifyConfig.headers.push({ for: '/couleur', values: { BLUE: 'bleu' } })
