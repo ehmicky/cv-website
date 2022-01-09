51ec3d3375b00146feb7be80e8fe5991fad08e15
ehmicky <ehmicky@gmail.com>
2022-01-09 11:30:10 +0100

    Update edge handler

--- netlify/edge-handlers/example.js
+++ netlify/edge-handlers/example.js
@@ -1,6 +1,12 @@
-import avg from "math-avg";
-
-export async function onRequest(ev) {
-  const number = avg([2, 4])
-  console.log(`${number} ${Date.now()}`);
+export function onRequest(event) {
+  if (event.requestMeta.url.pathname === "/secret") {
+    event.replaceResponse(
+      new Response("<h1>Access denied</h1>", {
+        headers: {
+          "content-type": "text/html",
+        },
+        status: 404,
+      })
+    );
+  }
 }
